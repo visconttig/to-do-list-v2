e712e297051f5db0aa6d52ad4f56a4b06193be80
Visconttig <visconttig@gmail.com>
2022-02-24 13:22:26 -0300

    Change Atlas password.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 
 const express = require("express");
+require("dotenv").config();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
